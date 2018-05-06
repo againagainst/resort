25e9c97931f0575ea2df306786d553e96e9d5f83
Sergey Belash <again.against@gmail.com>
2018-05-06 12:30:14 +0300

    Config & CLI options traking. Storing a basig etalon.

--- resort/etalons.py
+++ resort/etalons.py
@@ -1,3 +1,5 @@
+import pathlib
+import requests
 
 
 class BasicHTTPResponseEtalon:
@@ -11,10 +13,25 @@ class BasicHTTPResponseEtalon:
     Server: TornadoServer/5.0.2
     '''
 
-    def __init__(self, response):
+    def __init__(self, response: requests.Response):
         self._headers = response.headers
 
     def __str__(self):
         title = 'Response'
         content = "\n".join('{0}: {1}'.format(k, v) for k, v in self._headers.items())
         return '{title}\n{content}'.format(title=title, content=content)
+
+
+class EtalonIO:
+
+    def __init__(self, config: dict):
+        self.read_file = config.get('input')
+        self.write_file = config.get('output')
+
+    def save(self, etalon: BasicHTTPResponseEtalon, filepath: pathlib.Path=None):
+        filepath = filepath or self.write_file
+
+        with filepath.open(mode='w') as f:
+            f.write(str(etalon))
+            pass
+        pass
