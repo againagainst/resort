558a2094624d56a3e353f5f8d1fe69a53b011e0a
Sergey Belash <again.against@gmail.com>
2018-05-21 22:59:33 +0300

    Use google's docstring style

--- resort/base_client.py
+++ resort/base_client.py
@@ -9,16 +9,20 @@ from server_spec import ServerSpecReader
 class BasicClient(object):
 
     def __init__(self, server_url: str, spec_file: str=None):
+        """Establishes connection to the server with given url.
+        With spec_file provided it can make snapshot for each
+        entry described in the spec.
+        """
+
         self._server_url = server_url
         if spec_file is not None:
             self.server_spec = ServerSpecReader(spec_file=spec_file)
 
     def prepare(self):
-        '''
-        Load a API Spec to the client's ServerSpecReader.
+        """Load a API Spec to the client's ServerSpecReader.
         returns self to create a prepared client:
         client = BasicClient().prepare()
-        '''
+        """
         self.server_spec.prepare()
         return self
 
@@ -26,7 +30,7 @@ class BasicClient(object):
         for method, each_entry in self.server_spec.paths_and_methods():
             yield self.snapshot(each_entry, method)
 
-    def snapshot(self, entry: str, method: str, Cls=BasicHTTPResponseEtalon):
+    def snapshot(self, entry: str, method: str, Etalon=BasicHTTPResponseEtalon):
         url = urllib.parse.urljoin(self._server_url, entry)
-        return Cls(entry=entry,
-                   response=requests.request(method, url))
+        return Etalon(entry=entry,
+                      response=requests.request(method, url))
