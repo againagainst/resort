e77dd7bd0a87f6fdb99e6bbd0ba28a13b65a6ddd
Sergey Belash <again.against@gmail.com>
2018-07-26 12:04:16 +0300

    Source code of "how to use the API" example was updated

--- examples/api-unittest/test_all.py
+++ examples/api-unittest/test_all.py
@@ -1,6 +1,5 @@
 import unittest
 import pathlib
-import sys
 
 from resort.engine import ResortEngine
 from resort.project import ResortProject
@@ -9,10 +8,15 @@ from resort.project import ResortProject
 class TestBasicRESTServer(unittest.TestCase):
 
     def test_resort_all(self):
-        curdir = pathlib.Path(__file__)
-        self.assertTrue(curdir.is_file)
-        # rp = ResortProject()
-        # ResortEngine.check()
+        curfile = pathlib.Path(__file__)
+        self.assertTrue(curfile.is_file())
+
+        project_dir = curfile.parent
+        self.assertTrue(project_dir.is_dir())
+
+        rp = ResortProject(project_dir)
+        self.assertIsNotNone(rp)
+        ResortEngine.check(rp)
 
 
 if __name__ == '__main__':
